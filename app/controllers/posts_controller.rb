22d6dc0fc15b633573e71c4c51547608d5dc29ea
AyakaIto <163850457+00ayaka00@users.noreply.github.com>
2024-07-04 08:06:09 +0000

    デプロイ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,10 +13,13 @@ class PostsController < ApplicationController
   
   def index
     #@post = Post.page(params[:page])
-    @posts = Post.includes(:image).all
+   
+    @posts = Post.all
+   
   end
 
   def show
+    @post =  Post.find(params[:id])
   end
 
   def edit
