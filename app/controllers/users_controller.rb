8331fcab4b6b24742fb999e74b300b2790b7b073
AyakaIto <163850457+00ayaka00@users.noreply.github.com>
2024-07-08 12:25:25 +0000

    [Fix] Show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
-    @post =  Post.find(params[:id])
+    # @post =  Post.find(params[:id])
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
