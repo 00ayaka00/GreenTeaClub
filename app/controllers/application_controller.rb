3fb754783039a9b27b54073951340ecf3f199e4e
AyakaIto <163850457+00ayaka00@users.noreply.github.com>
2024-07-21 08:09:22 +0000

    user/shoe削除・編集ボタン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
 
+  before_action :authenticate_user!, except: [:top], unless: :admin_controller?
   before_action :configure_authentication
-  
-  
+
+
   private
-  
+
   def configure_authentication
     if admin_controller?
       authenticate_admin!
@@ -12,14 +13,14 @@ class ApplicationController < ActionController::Base
       authenticate_user! unless action_is_public?
     end
   end
- 
+
   def admin_controller?
     self.class.module_parent_name == 'Admin'
   end
-  
+
   def action_is_public?
     controller_name == 'homes' && action_name == 'top'
   end
-  
-  
+
+
 end
--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,6 +1,6 @@
 class Public::SearchesController < ApplicationController
   
-  before_action :authenticate_user!
+ 
   
    def search
     @range = params[:range]
