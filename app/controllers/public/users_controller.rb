473f5e23d10923473feb37dfd5a88eea043f2f52
AyakaIto <163850457+00ayaka00@users.noreply.github.com>
2024-09-09 12:20:45 +0000

    マイページ

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -12,7 +12,7 @@ class Public::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.page(params[:page])
-    @post =  Post.find(params[:id])
+    #@post =  Post.find(params[:id])
   end
 
   def edit
