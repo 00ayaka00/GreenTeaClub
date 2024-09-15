4bde552ca182dea7e28a19113946ef25a0fe3ed2
AyakaIto <163850457+00ayaka00@users.noreply.github.com>
2024-09-15 12:31:56 +0000

    admin

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -3,7 +3,7 @@ class Admin::DashboardsController < ApplicationController
   layout 'admin'
   before_action :authenticate_admin!
     def index
-        @users = User.all
+        @users = User.page(params[:page]).per(10)
     end
   
 end
